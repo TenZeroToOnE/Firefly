a0b77d8a7a788936984fb5f197e983c628bdbc01
xiaye <xiaye@msn.com>
2026-08-28 21:02:30 +0800

    站点url

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "blog",
 
 	// 站点 URL
-	site_url: "blog.shengxuanmin.workers.dev",
+	site_url: "https://blog.shengxuanmin.workers.dev",
 
 	// 站点描述
 	description:
