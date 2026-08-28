aa03c9926fec25079ef92b5a5d76ec21b64fc303
Tenone <Tenoneshiyi@gmail.com>
2026-08-29 00:26:04 +0800

    站点url

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "blog",
 
 	// 站点 URL
-	site_url: "https://blog.shengxuanmin.workers.dev",
+	site_url: "https://blog.tenone-759.workers.dev",
 
 	// 站点描述
 	description:
